3e5d8a082a03b464320cf12121fc44ee4c06a983
Gabriel Engel <gabriel.engel@fgsys.com>
2015-06-15 00:03:57 -0300

    Following official meteor guidelines

--- package.js
+++ package.js
@@ -1,20 +1,27 @@
+// package metadata file for Meteor.js
+'use strict';
+
+var packageName = 'autolinker:autolinker'; // https://atmospherejs.com/autolinker/autolinker
+var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
+
+var packageJson = JSON.parse(Npm.require("fs").readFileSync('package.json'));
+
 Package.describe({
-	name: 'autolinker:autolinker',
-	summary: "Official Autolinker.js packaged for Meteor.",
-	version: "1.0.2",
+	name: packageName,
+	summary: "Autolinker.js (official) - Automatically Link URLs, Email Addresses, Phone Numbers, Twitter handles, and Hashtags in a given block of text/HTML",
+	version: packageJson.version,
 	git: "https://github.com/gregjacobs/Autolinker.js.git"
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.0.2.1');
-	api.use('templating', 'client');
-	api.addFiles('dist/Autolinker.js', ['client', 'server']);
+	api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+	api.addFiles(['dist/Autolinker.js', 'meteor/export.js']);
 	api.addFiles('meteor/helpers.js', ['client']);
-	api.export("Autolinker");
+	api.export('Autolinker');
 });
 
 Package.onTest(function(api) {
+	api.use(packageName, where);
 	api.use('tinytest');
-	api.use('autolinker:autolinker');
-	api.addFiles('meteor/test.js', ['client']);
+	api.addFiles('meteor/test.js');
 });
