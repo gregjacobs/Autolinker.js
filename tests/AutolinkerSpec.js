325efb081413615858ffa1473674116f21b215ed
Gregory Jacobs <greg@greg-jacobs.com>
2016-06-28 21:57:47 -0400

    Add another test checking that no infinite loop is caused in Chrome by a `<` in the input that doesn't begin an HTML tag

--- tests/AutolinkerSpec.js
+++ tests/AutolinkerSpec.js
@@ -1283,6 +1283,17 @@ describe( "Autolinker", function() {
 			} );
 
 
+			it( "should not fail with an infinite loop for an input string with " +
+				"marginally valid HTML (issue #157)",
+			function() {
+				var str = "Our first step should be to get a CBC with differential, accompanied by a blood smear. The blood smear will be read (by a Hematologist) as sparse platelets among RBCs and some WBCs, that will most likely be normal. The platelets that do show up on the blood smear may or may not be damaged. In the case of TTP, platelets should not be damaged, but rather just low in number. A CBC with platelet count <50K starts to raise eyebrows for a possible case requiring platelet transfusion. Now would be the time to get a Type & Screen, and most would also tend towards ordering PT, PTT, and INR, or the \"coagulative\" measurement laboratory tests. Confirmation of TTP would be made by a serum ADAMST13 activity level."
+
+				var result = autolinker.link( str );
+
+				expect( result ).toBe( str );
+			} );
+
+
 
 			it( "should NOT modify the email address with other tags when inside another anchor", function() {
 				var input = [
