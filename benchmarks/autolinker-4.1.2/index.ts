8cdf3cd1a7a0fdb3a3e06dcc2a25a9f6106c8f58
Gregory Jacobs <greg@greg-jacobs.com>
2025-05-02 21:51:01 -0400

    Remove some unused benchmarks

--- benchmarks/autolinker-4.1.2/index.ts
+++
@@ -1,9 +0,0 @@
-import Autolinker from 'autolinker';
-import { inputText } from '../input-text';
-import { sharedAutolinkerConfig } from '../shared-autolinker-config';
-
-const autolinker = new Autolinker(sharedAutolinkerConfig);
-
-export function runAutolinker4_1_2() {
-    autolinker.link(inputText);
-}
