3b473c4ef90e07356e64bbe25768ad124f730514
Gregory Jacobs <greg@greg-jacobs.com>
2025-05-02 21:50:22 -0400

    Remove meteor files - last publish on meteor was 10 years ago

--- meteor/export.js
+++
@@ -1,3 +0,0 @@
-/*global window */ // Meteor creates a file-scope global for exporting. This comment prevents a potential JSHint warning.
-Autolinker = window.Autolinker; // eslint-disable-line no-undef
-delete window.Autolinker;
